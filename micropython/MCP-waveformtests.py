6a7333510119d04721f3b86978decf5f7a89e18c
Jacob Fifield <43118015+fifeJ@users.noreply.github.com>
2021-05-08 06:55:40 -0600

    Update MCP-waveformtests.py
    moved the waveform generation methods into a class named wavePacket and removed from code. They are now in a file called wavePackets.py. Although the timers are interacting correctly, something is off on the update function and the right frequency is not being output. Look into phase accumulator design.

--- micropython/MCP-waveformtests.py
+++ micropython/MCP-waveformtests.py
@@ -1,71 +1,26 @@
 
 from MCP49XX import MCP
-from machine import ADC, Pin 
-
-import math
-
+from machine import ADC, Pin, Timer
+from wavePackets import wavePacket
 
 resolution = 128
-offset = 1.65
+frequency = 100
 myMCP = MCP("MCP4901", 9)
 aInput = machine.ADC(26)
-oddlist = (1,3,5,7,9,11,13,15)
+myWavePacket = wavePacket(resolution, "sin", 1.65)
 
-#Sin Wave
-mySinList = []
-for i in range(resolution):
-    value = math.sin((i/(resolution-1) *2*math.pi))+offset
-    mySinList.insert(i, value)
-
-#Taylor Sawtooth Wave
-myCSawList = []
-for i in range(resolution):
-    x = i/(resolution-1)
-    value = 0
-    for n in range(10):
-        value = value + math.sin((n+1)*x*math.pi*2)/(2*(n+1))
-    value = value + offset
-    myCSawList.insert(i, value)
-
-#Taylor Approx Triangle Wave
-myTaylorTriangleList = []
-for i in range(resolution):
-    x = (i/(resolution-1))*2*math.pi
-    value = math.sin(x)- (math.sin(3*x)/9)
-    + (math.sin(5*x)/25)
-    - (math.sin(7*x)/49)
-    + (math.sin(9*x)/81)
-    - (math.sin(11*x)/121)
-    + (math.sin(13*x)/169)
-    value = value + offset
-    myTaylorTriangleList.insert(i, value)
+#Function to step through the values of a wave list
+soundTimer = Timer()
+freqTimer = Timer()
 
 
-#Taylor Square Wave 
-myTSquareList = []
-oddlist = [1,3,5,7,9,11]
-for i in range(resolution):
-    x = (i/resolution-1)*(2*math.pi)
-    value = 0
-    for n in oddlist:
-        value = value + (1/n)*(math.sin(x*n))
-    value = value+offset
-    myTSquareList.insert(i, value)
-
-   
-#Discontinuous Ramp Wave
-myDRampList = []
-for i in range(resolution):
-    value = -1 + (2*(i/(resolution-1))) + offset
-    myDRampList.insert(i, value)
-
-
+def playSound(soundTimer):
+    myMCP.writeVolt(myWavePacket.currentFrameValue)
+    myWavePacket.advanceFrame()
     
-beegList=[mySinList, myCSawList ,myTaylorTriangleList, myTSquareList,myDRampList]
-for lists in beegList:
-    print("Break")
-    for value in lists:
-        myMCP.writeVolt(value)
-        reading = aInput.read_u16()
-        reading = round(reading*(myMCP.vRef/65536), 3)
-        print(reading)
+def update(freqTimer):
+    myWavePacket.advanceFrame()
+    
+soundTimer.init(freq= 44100, mode=Timer.PERIODIC, callback=playSound)
+freqTimer.init(freq=frequency, mode=Timer.PERIODIC, callback=update)
+
